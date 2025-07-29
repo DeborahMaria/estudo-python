b6dade0b4f3300c9d7c1994a46eaed9111e00482
DeborahMaria <deborah.maria1@hotmail.com>
2025-07-28 22:56:07 -0300

    Atualiza programa 16 e adiciona novo programa 17

--- 16 PROGRAMA treinando_for_range_inicio_fim_passo.py.py
+++ 16 PROGRAMA treinando_for_range_inicio_fim_passo.py.py
@@ -1,3 +1,15 @@
 #treinando_for_range_inicio_fim_passo
 
 #a proposta é criar dois tipos de contagem e imprimir na tela , uma tera numeros pares de 0 a 10 e a segunda contagem tera de 1 a 9.
+#primeiro dou inicio ao for,em seguida adiciono uma "variavel" que ira interagir com o a listagem, "in" fazendo a ponte
+#e por fim adicionarei o range (inicio,fim,intervalo)
+print ("Contagem de numeros pares")
+for contador in range(0,11,2):
+    print(f"par: {contador}")
+    
+print ("Contagem de numeros impares")
+for contador in range(1,10,2):
+    print(f"impar: {contador}")
+    
+#o primeiro for conta de 0 a 10 de 2 em 2 assim evidenciando os numeros pares
+#o segundo for conta de 1 a 9 de 2 em 2 assim evidenciando os numeros impares
